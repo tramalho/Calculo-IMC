dc6dfa4ff03118738ff42050228244c8c9878f4a
tramalho <tramalho@ciandt.com>
2019-01-25 20:37:46 -0200

    Hora de programar: vamos calcular o IMC

--- Calculo IMC/ViewController.swift
+++ Calculo IMC/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tfResult: UILabel!
     @IBOutlet weak var ivResult: UIImageView!
     
+    @IBOutlet weak var ResultContainer: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,37 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: Any) {
+        
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!) {
+           let imc = weight / pow(height, 2)
+            showResults(imc: imc)
+        }
     }
 
-}
+    private func showResults(imc : Double){
+        var result = ""
+        var image = ""
 
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image  = "abaixo"
+            case 0..<16:
+                result = "Abaixo do Peso"
+                image  = "abaixo"
+            case 0..<16:
+                result = "Peso Ideal"
+                image  = "ideal"
+            case 0..<16:
+                result = "Sobrepeso"
+                image  = "sobre"
+            default:
+                result = "Obesidade"
+                image  = "obesidade"
+        }
+        
+        tfResult.text = result
+        ivResult.image = UIImage(named: image)
+        ResultContainer.isHidden = false
+    }
+}
